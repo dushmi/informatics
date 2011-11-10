f366f77dd208619cd5d93f64e289297160a49d7c
Mihai Dusmanu <dushmi94@gmail.com>
2011-11-10 19:16:31 +0200

    537@SGU WA

---
+++ SGU/537.cpp
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+char s[30];
+
+int n;
+
+long long f[30];
+
+long long gcd(long long x, long long y) {
+  long long r = x % y;
+
+  while (r) {
+    x = y;
+    y = r;
+    r = x % y;
+  }
+
+  return y;
+}
+
+void afis(long long x) {
+  vector <long long> v;
+  
+  for (long long i = 1; i * i <= x; ++i)
+    if (x % i == 0) {
+      v.push_back(i);
+
+      if (i != x / i)
+        v.push_back(x / i);
+    }
+  
+  sort(v.begin(), v.end());
+
+  for (int i = 0; i < (int)v.size(); ++i)
+    printf("%lld ", v[i]);
+
+  printf("\n");
+}
+
+void solve() {
+  n = strlen(s) - 1;
+
+  int nrd = 0, i = 0;
+
+  long long pas = 0, rez = 0;
+  
+  for (int i = 0; i < 26; ++i)
+    f[i] = 0;
+
+  for (i = n, pas = 1; i >= 0; --i, pas *= 10) {
+    if (!f[s[i] - 'a'])
+      ++nrd;
+  
+    f[s[i] - 'a'] += pas;
+  }
+
+  if (nrd < 10) {
+    for (i = 0; i < 26; ++i)
+      if (f[i]) {
+        if (!rez)
+          rez = f[i];
+        else
+          rez = gcd(rez, f[i]);
+      }
+
+    afis(rez);
+    
+    return;
+  }
+
+  sort(s, s + n + 1);
+
+  if (n == 9) {
+    printf("1 3 9\n");
+
+    return;
+  }
+
+  if (n == 12 && s[0] == s[1] && s[0] == s[2] && s[0] == s[3]) {
+    printf("1 3\n");
+    
+    return;
+  }
+
+  printf("1\n");
+}
+
+int main() {
+  int T;
+
+  scanf("%d\n", &T);
+
+  for (int i = 1; i <= T; ++i) {
+    gets(s);
+
+    printf("Case %d: ", i);
+
+    solve();
+  }
+
+  return 0;
+}
