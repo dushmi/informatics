9346d81a2a8a5e181b7f9ce0131db16ac9f5722a
Mihai Dusmanu <dushmi94@gmail.com>
2011-11-14 23:44:26 +0200

    alice@infoarena solved

---
+++ infoarena/alice.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+const int N = 35;
+
+double x, d[N][N];
+
+int n, r;
+
+void solve() {
+  d[0][0] = 1;
+
+  for (int i = 1; i <= n; ++i)
+    d[i][0] = 2 * d[i - 1][0];
+
+  for (int j = 1; j <= r; ++j)
+    d[0][j] = 2 * d[0][j - 1];
+
+  for (int i = 1; i <= n; ++i)
+    for (int j = 1; j <= r; ++j)
+      d[i][j] = d[i - 1][j] * (1 - ((d[i - 1][j] - d[i][j - 1]) / (d[i - 1][j] + d[i][j - 1])));
+}
+
+int main() {
+  freopen("alice.in", "r", stdin);
+  freopen("alice.out", "w", stdout);
+
+  scanf("%d%d%lf", &n, &r, &x);
+
+  solve();
+
+  printf("%lld", (long long)((double)d[n][r] * x));
+
+  return 0;
+}
