6a46b271a208b6b073826f57999c6624a6f5d662
Mihai Dusmanu <dushmi94@gmail.com>
2012-03-27 18:38:57 +0300

    tj@infoarena TLE

--- infoarena/tj.cpp
+++ infoarena/tj.cpp
@@ -4,16 +4,14 @@ const int N = 257;
 
 bool L[N][N];
 
-short int n, v[N], deg[N], tri[N][N];
+short int n, v[N], deg[N];
 
 void reset() {
   for (short int i = 0; i <= n; ++i) {
     deg[i] = 0;
 
-    for (short int j = 0; j <= n; ++j) {
+    for (short int j = 0; j <= n; ++j)
       L[i][j] = 0;
-      tri[i][j] = 0;
-    }
   }
 }
 
@@ -31,13 +29,20 @@ void read() {
   }
 }
 
-void init() {
-  for (short int i = 1; i <= n; ++i)
-    for (short int j = i + 1; j <= n; ++j)
-      if (L[i][j])
-        for (short int k = 1; k <= n; ++k)
-          if (i != k && j != k && L[i][k] && L[j][k])
-            ++tri[i][j], ++tri[j][i];
+bool nrtri(short int x, short int size) {
+  for (short int i = 0; i < size; ++i) 
+    if (L[x][v[i]]) {
+      short int rez = 0;
+
+      for (short int j = 0; j < size; ++j)
+        if (L[x][v[j]] && L[v[i]][v[j]])
+          ++rez;
+    
+      if (rez == deg[x] - 1)
+        return 1;
+    }
+  
+  return 0;
 }
 
 void solve() {
@@ -52,23 +57,15 @@ void solve() {
     ok = 0;
 
     for (short int i = 0; i < size && !ok; ++i)
-      for (short int j = 0; j < size && !ok; ++j)
-        if (v[i] != v[j] && L[v[i]][v[j]]) {
-          if (tri[v[i]][v[j]] == deg[v[i]] - 1) {
-            for (short int k = 0; k < size; ++k)
-              if (L[v[i]][v[k]])
-                --deg[v[k]];
+      if (nrtri(v[i], size)) {
+        for (short int k = 0; k < size; ++k)
+          if (L[v[i]][v[k]])
+            --deg[v[k]];
             
-            for (short int k = 0; k < size; ++k)
-              for (short int l = k + 1; l < size; ++l)
-                if (L[v[k]][v[l]] && L[v[i]][v[k]] && L[v[i]][v[l]])
-                  --tri[v[k]][v[l]], --tri[v[l]][v[k]];
-
-            ok = 1;
-            v[i] = v[size - 1];
-            --size;
-          }
-        }
+        ok = 1;
+        v[i] = v[size - 1];
+        --size;
+      }
   }
 
   if (size > 1) 
@@ -89,8 +86,6 @@ int main() {
     reset();
 
     read();
-
-    init();
 
     solve();
   }
