eae6035a269ac363c47e810d4fb6a1deca3cf9c0
Mihai Dusmanu <dushmi94@gmail.com>
2012-04-10 17:50:50 +0300

    tj@infoarena solved

--- infoarena/tj.cpp
+++ infoarena/tj.cpp
@@ -2,15 +2,14 @@
 
 const int N = 257;
 
-bool L[N][N];
+bool fr[N], L[N][N];
 
-short int n, v[N], deg[N];
+int n, v[N];
 
 void reset() {
-  for (short int i = 0; i <= n; ++i) {
-    deg[i] = 0;
-
-    for (short int j = 0; j <= n; ++j)
+  for (int i = 1; i <= n; ++i) {
+    fr[i] = 0;
+    for (int j = 1; j <= n; ++j)
       L[i][j] = 0;
   }
 }
@@ -18,54 +17,42 @@ void reset() {
 void read() {
   int m, x, y;
 
-  scanf("%hd%d", &n, &m);
+  scanf("%d%d", &n, &m);
 
   for (int i = 1; i <= m; ++i) {
     scanf("%d%d", &x, &y);
-
+    
     L[x][y] = L[y][x] = 1;
-    ++deg[x];
-    ++deg[y];
   }
 }
 
-bool nrtri(short int x, short int size) {
-  for (short int i = 0; i < size; ++i) 
-    if (L[x][v[i]]) {
-      short int rez = 0;
-
-      for (short int j = 0; j < size; ++j)
-        if (L[x][v[j]] && L[v[i]][v[j]])
-          ++rez;
-    
-      if (rez == deg[x] - 1)
-        return 1;
-    }
+bool valid(int x, int y) {
+  for (int i = 1; i <= n; ++i) 
+    if (L[y][i] && !fr[i] && i != x)
+      if (!L[x][i])
+        return 0;
   
-  return 0;
+  return 1;
 }
 
 void solve() {
-  short int size = 0;
-  
-  for (short int i = 1; i <= n; ++i)
-    v[size++] = i;
-  
   bool ok = 1;
+  
+  int size = n;
 
   while (size >= 2 && ok) {
     ok = 0;
 
-    for (short int i = 0; i < size && !ok; ++i)
-      if (nrtri(v[i], size)) {
-        for (short int k = 0; k < size; ++k)
-          if (L[v[i]][v[k]])
-            --deg[v[k]];
-            
-        ok = 1;
-        v[i] = v[size - 1];
-        --size;
-      }
+    for (int i = 1; i <= n; ++i)
+      for (int j = 1; j <= n; ++j)
+        if (!fr[i] && !fr[j] && L[i][j] && valid(i, j)) {
+          --size;
+          fr[j] = 1;
+          ok = 1;
+
+          for (int k = 1; k <= n; ++k)
+            L[k][j] = 0;
+        }
   }
 
   if (size > 1) 
@@ -78,11 +65,11 @@ int main() {
   freopen("tj.in", "r", stdin);
   freopen("tj.out", "w", stdout);
 
-  short int T;
+  int T;
 
-  scanf("%hd", &T);
+  scanf("%d", &T);
 
-  for (short int i = 1; i <= T; ++i) {
+  for (int i = 1; i <= T; ++i) {
     reset();
 
     read();
